d6c08b03044b4cac0757b35005506c3527a82ee8
ghkdwlgh <dj20250621@dge.go.kr>
2026-03-26 14:39:40 +0900

    Add files via upload

---
+++ S4_0403.java
@@ -0,0 +1,13 @@
+package jump2java;
+
+public class S4_0403 {
+	 public static void main(String[] args) {
+		 for(int i=2; i<10; i++) {
+			    for(int j=1; j<10; j++) {
+			        System.out.print(i*j+" ");
+			    }
+			    System.out.println("");  
+			}
+
+	 }
+}
