cc27678aaba1fb62c30cb882a08f3b4f20c6d31a
ghkdwlgh <dj20250621@dge.go.kr>
2026-04-01 09:43:41 +0900

    Add files via upload

---
+++ S5_0204.java
@@ -0,0 +1,18 @@
+package jump2java;
+class Animal1 {
+    String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+public class S5_0204 {
+	public static void main(String[] args) {
+        Animal1 cat = new Animal1();
+        cat.setName("boby");
+
+        Animal1 dog = new Animal1();
+        dog.setName("happy");
+    }
+}
+
