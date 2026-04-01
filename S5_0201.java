80c15440f3cd5d954383b38cc210df7eef635c68
ghkdwlgh <dj20250621@dge.go.kr>
2026-04-01 09:37:45 +0900

    Add files via upload

---
+++ S5_0201.java
@@ -0,0 +1,11 @@
+package jump2java;
+class Animal {
+	String name;
+}
+public class S5_0201 {
+	public static void main(String[] args) {
+        Animal cat = new Animal();
+        System.out.println(cat.name);
+    }
+}
+	 
