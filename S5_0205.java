95f88fed2492b56c9ee1bd616c96fcf599874f4f
ghkdwlgh <dj20250621@dge.go.kr>
2026-04-01 09:44:57 +0900

    Add files via upload

---
+++ S5_0205.java
@@ -0,0 +1,19 @@
+package jump2java;
+class Animal {
+    String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class S5_0205 {
+	class Animal {
+	    String name;
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	}
+
+}
