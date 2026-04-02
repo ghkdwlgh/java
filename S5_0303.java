2fed2141a717b3ea6d0add6e3485522d75a8726e
ghkdwlgh <dj20250621@dge.go.kr>
2026-04-02 09:10:33 +0900

    Add files via upload

---
+++ S5_0303.java
@@ -0,0 +1,14 @@
+package jump2java;
+
+public class S5_0303 {
+	 String say() {
+	        return "Hi";
+	    }
+
+	    public static void main(String[] args) {
+	        S5_0303 sample = new S5_0303();
+	        String a = sample.say();
+	        System.out.println(a);  // "Hi" 출력
+	    }
+	}
+
