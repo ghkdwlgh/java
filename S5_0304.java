9207e6f10d988d414d6e2ae30f59603d73638201
ghkdwlgh <dj20250621@dge.go.kr>
2026-04-02 09:11:52 +0900

    Add files via upload

---
+++ S5_0304.java
@@ -0,0 +1,13 @@
+package jump2java;
+
+public class S5_0304 {
+	 void sum(int a, int b) {
+	        System.out.println(a+"과 "+b+"의 합은 "+(a+b)+"입니다.");
+	    }
+
+	    public static void main(String[] args) {
+	        S5_0304 sample = new S5_0304();
+	        sample.sum(3, 4);
+	    }
+	}
+
